bdba74cc936290aa9c3c1698c61c2326f79e3e69
RevanthR08 <revanthofficial08@gmail.com>
2024-06-01 16:02:57 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import LoginForm from "./comforlog/Login/LoginForm";
+ 
+function App() {
+  return (
+    <>
+     <div className="App">
+      <LoginForm/>
+     </div>
+    </>
+  );
+}
+
+export default App;
